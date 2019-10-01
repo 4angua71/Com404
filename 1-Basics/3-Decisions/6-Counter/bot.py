8ca2fc4021a29ef056545a94ca838e7b892ff0ef
4angua71 <4angua71@solent.ac.uk>
2019-10-01 09:33:08 +0000

    Added code to demonstrate how a variable can be used as a counter.

---
+++ 1-Basics/3-Decisions/6-Counter/bot.py
@@ -0,0 +1,21 @@
+Even_count = 0
+Odd_count = 0
+
+first = int(input("Please enter the first number: "))
+second = int(input("Please enter the second number: "))
+third = int(input("Please enter the third number: "))
+
+if(first % 2 == 0):
+     Even_count = Even_count + 1
+else:
+    Odd_count = Odd_count + 1
+if(second % 2 == 0):
+     Even_count = Even_count + 1
+else:
+    Odd_count = Odd_count + 1
+if(third % 2 == 0):
+     Even_count = Even_count + 1
+else:
+    Odd_count = Odd_count + 1
+
+print("There were ", Even_count, "numbers and " , Odd_count, "numbers" )
