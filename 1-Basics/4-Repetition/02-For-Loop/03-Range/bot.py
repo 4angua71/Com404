490d647c441f9717ccb560c1c4a5caa1fe7796fe
4angua71 <4angua71@solent.ac.uk>
2019-10-08 10:25:32 +0000

    Added code to demonstrate the use of a simple for loop.

---
+++ 1-Basics/4-Repetition/02-For-Loop/03-Range/bot.py
@@ -0,0 +1,11 @@
+N  = int(input("What level of brightness is required?: "))
+
+
+print("Adjusting brightness...")
+for count in range(2, N+1, 2):
+    print()
+    print("Beep's brightness level: " + "*" * count)
+    print("Bop's brightness level: " + "*" * count)
+    
+print("Adjustments complete!")
+
